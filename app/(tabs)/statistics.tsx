b7c777b97da51c9bb8ba3ee39132a5cff7f021a6
Nitin <rajnitin793@gmail.com>
2025-12-17 09:33:27 +0530

    added segmented control

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,36 +1,88 @@
-import ScreenWrapper from '@/components/ScreenWrapper'
-import Typo from '@/components/Typo'
-import React from 'react'
-import { Image, StyleSheet, View } from 'react-native'
+import Header from "@/components/Header";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { scale, verticalScale } from "@/utils/styling";
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import SegmentedControlTab from "react-native-segmented-control-tab";
 
 const Statistics = () => {
+  const [activeindex, setActiveIndex] = useState(0);
+
   return (
     <ScreenWrapper>
       <View style={styles.container}>
-        <Image
-          source={require("../../assets/images/developing.gif")}
-          style={styles.devImage}
-        />
-        <Typo   >
-          Module Under Active Development
-        </Typo>
+        <View style={styles.header}>
+          <Header title="Statistics" />
+        </View>
+
+        <ScrollView
+          contentContainerStyle={{
+            gap: spacingY._20,
+            paddingTop: spacingY._5,
+            paddingBottom: verticalScale(100),
+          }}
+          showsVerticalScrollIndicator={false}
+        >
+          <SegmentedControlTab
+            values={["Weekly", "Monthly", "Yearly"]}
+            selectedIndex={activeindex}
+            onTabPress={(index: number) => {
+              setActiveIndex(index);
+            }}
+          />
+        </ScrollView>
       </View>
     </ScreenWrapper>
-  )
-}
+  );
+};
 
-export default Statistics
+export default Statistics;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  chartContainer: {
+    position: "relative",
     justifyContent: "center",
     alignItems: "center",
   },
-  devImage: {
-    width: 250,
-    height: 250,
-    resizeMode: "contain",
-    marginBottom: 40
-  }
-})
+
+  chartLoadingContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    borderRadius: radius._12,
+    backgroundColor: "rgba(0,0,0,0.6)",
+  },
+
+  header: {},
+
+  noChart: {
+    backgroundColor: "rgba(0,0,0,0.6)",
+    height: verticalScale(210),
+  },
+
+  searchIcon: {
+    backgroundColor: colors.neutral700,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100,
+    height: verticalScale(35),
+    width: verticalScale(35),
+    borderCurve: "continuous",
+  },
+  segmentStyle: {
+    height: scale(37),
+  },
+
+  segmentFontStyle: {
+    fontSize: verticalScale(13),
+    fontWeight: "bold",
+    color: colors.black,
+  },
+
+  container: {
+    paddingHorizontal: spacingX._20,
+    paddingVertical: spacingY._5,
+    gap: spacingY._10,
+  },
+});
